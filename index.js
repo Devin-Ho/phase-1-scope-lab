afbb20054f6f4fadb88546a6fde041226d6e05f7
Devin Ho <djxho36@gmail.com>
2022-04-09 11:08:31 -0400

    commit tree c471367e079e9a6cf994090bbe3d1d67d04fafd2

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();  
+}
+
+var bestCustomer;
+function setBestCustomer() {
+    return bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCharacter = "tod";
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCharacter = "mary";
+}
+
